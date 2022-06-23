537cd6b7f63273acc97a17b342eba9f1f667cac8
ke1echi <mrkelechichinaka@gmail.com>
2022-06-23 15:33:31 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	routes "github.com/kapbyte/golang-jwt-project/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("/api-1", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong-api-1",
+		})
+	})
+
+	router.Run(":" + port)
+}
---
+++ routes/authRouter.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	controllers "github.com/kapbyte/golang-jwt-project/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("users/signup", controllers.Signup())
+	incomingRoutes.POST("users/login", controllers.Login())
+}
